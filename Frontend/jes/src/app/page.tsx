8b3ab072a122d38a918ebcba51a9b49c0efa6359
tail-s <lunar7s@naver.com>
2023-07-31 19:20:12 +0900

    test

--- Frontend/jes/src/app/page.tsx
+++ Frontend/jes/src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <main className={styles.main}>
       <div className={styles.description}>
         <p>
-          Final Test ( Auto Image Delete )
+          하하하하하하
           Get started by editing&nbsp;
           <code className={styles.code}>src/app/page.tsx</code>
         </p>
