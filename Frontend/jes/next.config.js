d5b34773f8d90152b78d61f782c9004e9f25ef0b
tail-s <lunar7s@naver.com>
2023-08-04 22:11:24 +0900

    webhook test

--- Frontend/jes/next.config.js
+++ Frontend/jes/next.config.js
@@ -6,5 +6,5 @@ module.exports = nextConfig
 const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = {
-    assetPrefix: isProd ? 'http://sojin.ddns.net:3000' : '',
+    assetPrefix: isProd ? 'http://sojin.ddns.net:13000' : '',
 }
