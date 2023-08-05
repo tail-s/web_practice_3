018d7213571168e8e66587727511e20bf994e0f6
tail-s <lunar7s@naver.com>
2023-08-06 02:36:21 +0900

    img test

--- Frontend/jes/next.config.js
+++ Frontend/jes/next.config.js
@@ -5,21 +5,6 @@ module.exports = nextConfig
 
 const isProd = process.env.NODE_ENV === 'production';
 
-// module.exports = {
-//     assetPrefix: isProd ? 'http://sojin.ddns.net:13000' : '',
-// };
-
 module.exports = {
-  assetPrefix: isProd ? 'http://sojin.ddns.net:13000' : '',
-  webpack: (config, { isServer }) => {
-    // isServer가 true인 경우 서버 측 렌더링에 사용되는 웹팩 설정입니다.
-    // isServer가 false인 경우 클라이언트 측 렌더링에 사용되는 웹팩 설정입니다.
-
-    // 프로덕션 환경인 경우 output.publicPath를 설정합니다.
-    if (isProd) {
-      config.output.publicPath = 'http://sojin.ddns.net:13000' + config.output.publicPath;
-    }
-
-    return config;
-  },
+    assetPrefix: isProd ? 'http://sojin.ddns.net:13000' : '',
 };
