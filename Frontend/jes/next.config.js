9438cd58b8a48342c200c518a9120808eb029fba
tail-s <lunar7s@naver.com>
2023-08-07 00:15:52 +0900

    DELETE NGINX

--- Frontend/jes/next.config.js
+++ Frontend/jes/next.config.js
@@ -6,5 +6,5 @@ module.exports = nextConfig
 const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = {
-    assetPrefix: isProd ? 'http://jes.ddns.net:13000' : '',
+    assetPrefix: isProd ? 'http://218.101.140.118:13000' : '',
 };
