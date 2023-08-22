55137a19212fed3de82f89dbe87b42e86abb8b70
tail-s <lunar7s@naver.com>
2023-08-22 21:21:30 +0900

    test

--- Frontend/jes/next.config.js
+++ Frontend/jes/next.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-module.exports = nextConfig
-
 const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = {
     assetPrefix: isProd ? `${process.env.FRONT_URL}` : '',
 };
+
+module.exports = nextConfig
